b9cf487a553105678607fdf50e851878580cc728
Agni Nugroho <agniwwe@gmail.com>
2024-07-03 14:35:00 +0700

    modified:   src/main/java/com/app/Controller/RegisterController.java 	modified:   src/main/resources/FXML/login.fxml 	modified:   src/main/resources/FXML/register.fxml 	modified:   src/main/resources/Style/login.css 	modified:   target/classes/FXML/login.fxml 	modified:   target/classes/FXML/register.fxml 	modified:   target/classes/Style/login.css 	modified:   target/classes/com/app/Controller/RegisterController.class

--- src/main/java/com/app/Controller/RegisterController.java
+++ src/main/java/com/app/Controller/RegisterController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import com.app.Model.Model;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
 public class RegisterController {
     
@@ -21,11 +23,31 @@ public class RegisterController {
         String password = passwordField.getText();
         BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/CSV/Account.csv", true));
 
-        writer.append("\n" + username + "," + password);
-        writer.close();
+        if (username.isEmpty() || password.isEmpty()) {
+            Alert alertWarning = new Alert(AlertType.WARNING);
+            alertWarning.setTitle("Warning");
+            alertWarning.setHeaderText("Empty Fields");
+            alertWarning.setContentText("Please fill in all fields");
+            alertWarning.showAndWait();
+        } else {
+            Alert alertConf = new Alert(AlertType.CONFIRMATION);
+            alertConf.setTitle("Confirmation");
+            alertConf.setHeaderText("Account Confirmation");
+            alertConf.setContentText("Are you sure all the information is correct?");
+            alertConf.showAndWait();
+            if (alertConf.getResult().getText().equals("OK")) {
+                writer.write("\n" + username + "," + password);
+                writer.close();
+                Alert alertInfo = new Alert(AlertType.INFORMATION);
+                alertInfo.setTitle("Information");
+                alertInfo.setHeaderText("Account Created");
+                alertInfo.setContentText("Account has been created successfully");
+                alertInfo.showAndWait();
 
-        Stage stage = (Stage) usernameField.getScene().getWindow();
-        Model.getInstance().getViewFactory().removeStage(stage);
-        Model.getInstance().getViewFactory().showLoginWindow();
+                Stage stage = (Stage) usernameField.getScene().getWindow();
+                Model.getInstance().getViewFactory().removeStage(stage);
+                Model.getInstance().getViewFactory().showLoginWindow();
+            }
+        }
     }
 }
